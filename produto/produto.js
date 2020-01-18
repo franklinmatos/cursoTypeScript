533bd6763b9ac4952a716afd76648aaeaf7d6ab4
Franklin Correia <franklinmatos@gmail.com>
2020-01-18 14:03:55 -0300

    incluindo funções no produto

--- produto/produto.js
+++ produto/produto.js
@@ -8,12 +8,23 @@ class Produto {
         this.preco = preco;
         this.desconto = desconto;
     }
+    aplicarDesconto() {
+        let valorDesconto = this.preco * this.desconto;
+        console.log(" Chamando a função aplicarDesconto.....");
+        console.log('O valor do produto ', this.nome, ' é R$: ', this.preco, ' e o valor com desconto é R$: ', valorDesconto);
+    }
+    resumo() {
+        return `${this.nome} cusa R$${this.preco} (${this.desconto * 100}% off)`;
+    }
 }
-const produto01 = new Produto("Caneta Bic Preta", 4.99);
+const produto01 = new Produto("Caneta Bic Preta", 0.2);
 produto01.desconto = 0.06;
 console.log("### Produto 01 ###");
 console.log(produto01);
-const produto02 = new Produto("Carderno Escolar", 18.99, 4.50);
+const produto02 = new Produto("Carderno Escolar", 18.90, 0.2);
 console.log("### Produto 02 ###");
 console.log(produto02);
+console.log("### Produto 02 Resumo ###");
+console.log(produto02.resumo());
+produto02.aplicarDesconto();
 //# sourceMappingURL=produto.js.map
--- produto/produto.ts
+++ produto/produto.ts
@@ -5,13 +5,26 @@ class Produto{
         this.preco = preco
         this.desconto = desconto
     }
+
+    public  aplicarDesconto(): void {
+        let valorDesconto: number = this.preco * this.desconto;
+        console.log(" Chamando a função aplicarDesconto.....")
+        console.log('O valor do produto ',this.nome, ' é R$: ',this.preco, ' e o valor com desconto é R$: ',valorDesconto);
+    }
+
+    public resumo():string{
+        return `${this.nome} cusa R$${this.preco} (${this.desconto*100}% off)`
+    }
 }
 
-const produto01 = new Produto("Caneta Bic Preta", 4.99);
+const produto01 = new Produto("Caneta Bic Preta", 0.2);
 produto01.desconto = 0.06
 console.log("### Produto 01 ###");
 console.log(produto01);
 
-const produto02 = new Produto("Carderno Escolar",18.99,4.50);
+const produto02 = new Produto("Carderno Escolar",18.90,0.2);
 console.log("### Produto 02 ###");
 console.log(produto02);
+console.log("### Produto 02 Resumo ###");
+console.log(produto02.resumo());
+produto02.aplicarDesconto();
