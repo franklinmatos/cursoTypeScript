b24f6f9d628c7974163ddb9d07e28300c28b5116
Franklin M Correia <franklinmatos@gmail.com>
2020-01-24 05:36:59 -0300

    Aula respondendo ao desafio

--- classe/desafios.ts
+++ classe/desafios.ts
@@ -1,57 +1,81 @@
 // Exercício 1 - Classe
-function Moto(nome) {
-    this.nome = nome
-    this.velocidade = 0
- 
-    this.buzinar = function() {
-        console.log('Toooooooooot!')
+// function Moto(nome) {
+//     this.nome = nome
+//     this.velocidade = 0
+
+//     this.buzinar = function() {
+//         console.log('Toooooooooot!')
+//     }
+
+//     this.acelerar= function(delta) {
+//         this.velocidade = this.velocidade + delta
+//     }
+// }
+
+class Moto {
+    constructor(public nome: string, public velocidade: number = 0) {
+        this.nome = nome
+        this.velocidade = velocidade
     }
- 
-    this.acelerar= function(delta) {
-        this.velocidade = this.velocidade + delta
+
+    buzinar() {
+        console.log("BiBi")
+    }
+
+    acelerar(valor: number) {
+        this.velocidade += valor
+
     }
 }
- 
-var moto = new Moto('Ducati')
+
+const moto = new Moto('Ducati')
 moto.buzinar()
 console.log(moto.velocidade)
 moto.acelerar(30)
 console.log(moto.velocidade)
- 
+
 // Exercício 2 - Herança
-var objeto2D = {
-    base: 0,
-    altura: 0
+abstract class Objeto2D {
+    public altura: number = 0
+    public base: number = 0
+
+    abstract area(): number
 }
- 
-var retangulo = Object.create(objeto2D)
+// var objeto2D = {
+//     base: 0,
+//     altura: 0
+// }
+
+class Retangulo extends Objeto2D {
+    area(): number {
+        return this.base * this.altura
+    }
+}
+// var retangulo = Object.create(objeto2D)
+const retangulo = new Retangulo
 retangulo.base = 5
 retangulo.altura = 7
-retangulo.area = function() {
-    return this.base * this.altura
-}
-console.log(retangulo.area())
- 
+// retangulo.area = function () {
+//     return this.base * this.altura
+// }
+console.log("Área do Retangulo = ", retangulo.area())
+
 // Exercício 3 - Getters & Setters
-var estagiario = {
-    _primeiroNome: ''
-}
- 
-Object.defineProperty(estagiario, 'primeiroNome', {
-    get: function () {
+class Estagiario {
+    private _primeiroNome: string = ''
+    get primeiroNome(): string {
         return this._primeiroNome
-    },
-    set: function (valor) {
-        if (valor.length >= 3) {
-            this._primeiroNome = valor
+    }
+
+    set primeiroNome(nome: string) {
+        if (nome.length >= 3) {
+            this._primeiroNome = nome
         } else {
             this._primeiroNome = ''
         }
-    },
-    enumerable: true,
-    configurable: true
-})
- 
+    }
+}
+const estagiario = new Estagiario
 console.log(estagiario.primeiroNome)
 estagiario.primeiroNome = 'Le'
 console.log(estagiario.primeiroNome)
