942a6bbc31d8372b3fc7c7c417fbeeea47f2cb38
Franklin Correia <franklinmatos@gmail.com>
2020-01-27 13:47:59 -0300

    aula sobre namespace e inicio da aula sobre modulos

--- namespace/namespace.js
+++ namespace/namespace.js
@@ -1,21 +1,6 @@
 "use strict";
-var Geometria;
-(function (Geometria) {
-    let Area;
-    (function (Area) {
-        const PI = 3.14;
-        function circunferencia(raio) {
-            return PI * Math.pow(raio, 2);
-        }
-        Area.circunferencia = circunferencia;
-        function retangulo(base, altura) {
-            return base * altura;
-        }
-        Area.retangulo = retangulo;
-    })(Area = Geometria.Area || (Geometria.Area = {}));
-})(Geometria || (Geometria = {}));
-const PI = 2.99;
+///<reference path="geometriaCirc.ts"/>
+///<reference path="geometriaRect.ts"/>
 console.log(Geometria.Area.circunferencia(10));
 console.log(Geometria.Area.retangulo(10, 20));
-console.log(PI);
 //# sourceMappingURL=namespace.js.map
--- namespace/namespace.ts
+++ namespace/namespace.ts
@@ -1,20 +1,4 @@
-namespace Geometria {
-
-    export namespace Area {
-            const PI = 3.14
-
-            export function circunferencia(raio: number): number {
-                return PI * Math.pow(raio,2)
-            }
-            export function retangulo( base: number, altura: number): number {
-                return base * altura
-            }
-    }
-}
-
-const PI = 2.99
-
+///<reference path="geometriaCirc.ts"/>
+///<reference path="geometriaRect.ts"/>
 console.log(Geometria.Area.circunferencia(10))
 console.log(Geometria.Area.retangulo(10,20))
-
-console.log (PI)
