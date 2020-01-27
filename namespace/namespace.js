307602f9f31e86f90fc507b1ea5a83d8e0a3feaa
Franklin Correia <franklinmatos@gmail.com>
2020-01-27 13:01:43 -0300

    aula de namespaces aninhados

--- namespace/namespace.js
+++ namespace/namespace.js
@@ -1,2 +1,21 @@
 "use strict";
+var Geometria;
+(function (Geometria) {
+    let Area;
+    (function (Area) {
+        const PI = 3.14;
+        function circunferencia(raio) {
+            return PI * Math.pow(raio, 2);
+        }
+        Area.circunferencia = circunferencia;
+        function retangulo(base, altura) {
+            return base * altura;
+        }
+        Area.retangulo = retangulo;
+    })(Area = Geometria.Area || (Geometria.Area = {}));
+})(Geometria || (Geometria = {}));
+const PI = 2.99;
+console.log(Geometria.Area.circunferencia(10));
+console.log(Geometria.Area.retangulo(10, 20));
+console.log(PI);
 //# sourceMappingURL=namespace.js.map
