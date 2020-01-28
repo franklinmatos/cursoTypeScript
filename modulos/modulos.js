804483096919d7169f491004fb65009716bbb585
Franklin M Correia <franklinmatos@gmail.com>
2020-01-28 05:32:03 -0300

    aula sobre modulos, configuracao de biblioteca SystemJS para carregamento de modulos para serem interpretados pelo browser

--- modulos/modulos.js
+++ modulos/modulos.js
@@ -1,2 +1,8 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const retangulo_1 = require("./retangulo");
+const circunferencia_1 = require("./circunferencia");
+console.log('Carregou os modulos');
+console.log(retangulo_1.areaRetangulo(7, 8));
+console.log(circunferencia_1.areaCircunferencia(10));
 //# sourceMappingURL=modulos.js.map
---
+++ modulos/modulos.ts
@@ -0,0 +1,7 @@
+import { areaRetangulo } from './retangulo'
+import { areaCircunferencia } from './circunferencia'
+
+
+console.log('Carregou os modulos')
+console.log(areaRetangulo(7, 8))
+console.log(areaCircunferencia(10))
