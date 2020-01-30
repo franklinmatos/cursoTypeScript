065ad0490e667e3c1965ed994812fdbe80f18b9a
Franklin M Correia <franklinmatos@gmail.com>
2020-01-30 05:36:06 -0300

    criando array usando generics[1]

--- generics/generics.js
+++ generics/generics.js
@@ -5,7 +5,7 @@ function echo(objeto) {
 console.log(echo('João').length);
 console.log(echo(27).length);
 console.log(echo({ nome: 'João', idade: 27 }));
-//
+// Usando Generics
 function echoMelhorado(objeto) {
     return objeto;
 }
@@ -13,4 +13,9 @@ console.log(echoMelhorado('João').length);
 // console.log(echoMelhorado(27).length) // exibe erro na função length
 console.log(echoMelhorado(27));
 console.log(echoMelhorado({ nome: 'João', idade: 27 }));
+//Generics disponíveia na API
+const avaliacoes = [10, 7, 6];
+avaliacoes.push(8.8);
+// avaliacoes.push('5.5') assim que eu coloquei o tipo no array a string 5.5 foi invalidada pela API
+console.log(avaliacoes);
 //# sourceMappingURL=generics.js.map
--- generics/generics.ts
+++ generics/generics.ts
@@ -6,7 +6,7 @@ console.log(echo('João').length)
 console.log(echo(27).length)
 console.log(echo({ nome: 'João', idade: 27 }))
 
-//
+// Usando Generics
 function echoMelhorado<T>(objeto: T): T {
     return objeto
 }
@@ -16,3 +16,11 @@ console.log(echoMelhorado('João').length)
 // console.log(echoMelhorado(27).length) // exibe erro na função length
 console.log(echoMelhorado<number>(27))
 console.log(echoMelhorado({ nome: 'João', idade: 27 }))
+
+
+//Generics disponíveia na API
+const avaliacoes: Array<number> = [10, 7, 6]
+avaliacoes.push(8.8)
+// avaliacoes.push('5.5') assim que eu coloquei o tipo no array a string 5.5 foi invalidada pela API
+
+console.log(avaliacoes)
