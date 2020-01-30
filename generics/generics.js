371b85d29761c8b9458cd4e85e6ba9411059b20e
Franklin M Correia <franklinmatos@gmail.com>
2020-01-30 05:26:22 -0300

    criando função Generics

--- generics/generics.js
+++ generics/generics.js
@@ -5,4 +5,12 @@ function echo(objeto) {
 console.log(echo('João').length);
 console.log(echo(27).length);
 console.log(echo({ nome: 'João', idade: 27 }));
+//
+function echoMelhorado(objeto) {
+    return objeto;
+}
+console.log(echoMelhorado('João').length);
+// console.log(echoMelhorado(27).length) // exibe erro na função length
+console.log(echoMelhorado(27));
+console.log(echoMelhorado({ nome: 'João', idade: 27 }));
 //# sourceMappingURL=generics.js.map
--- generics/generics.ts
+++ generics/generics.ts
@@ -5,3 +5,14 @@ function echo(objeto: any) {
 console.log(echo('João').length)
 console.log(echo(27).length)
 console.log(echo({ nome: 'João', idade: 27 }))
+
+//
+function echoMelhorado<T>(objeto: T): T {
+    return objeto
+}
+
+
+console.log(echoMelhorado('João').length)
+// console.log(echoMelhorado(27).length) // exibe erro na função length
+console.log(echoMelhorado<number>(27))
+console.log(echoMelhorado({ nome: 'João', idade: 27 }))
